f6ca4bc729991ba4bbde059651471f2491099ec0
alkatsalex <alkatsalex@ukr.net>
2024-01-23 22:16:18 +0200

    hz4

--- src/store/store.js
+++ src/store/store.js
@@ -1,8 +1,8 @@
 import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import { contactReducer } from './ContactSlice/ContactSlice';
 import { filterReducer } from './filterSlice/filterSlice';
-import { loadingReduser } from './Loading/loadingSlice';
-import { registeReduser } from './Registration/registerSlice';
+import { loadingReduser } from './Loading/LoadingSlice';
+import { registeReduser } from './Registration/RegisterSlice';
 
 import { persistStore, persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
